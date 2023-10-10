dd6c8bc47716958700cda0ea4e6f0575914cc283
thecodephilic-guy <65657371+thecodephilic-guy@users.noreply.github.com>
2023-10-10 18:26:36 +0000

    Added binarySearch

---
+++ code/Array/binarySearch.java
@@ -0,0 +1,38 @@
+//Program to implement binary search
+//prerequisite - sorted array
+
+public class binarySearch{
+
+    public static int binSearch(int array[], int key){
+        int start = 0;
+        int end = array.length-1;
+
+        while (start<=end) {
+            int mid = start + ((end-start))/2;
+
+            if(array[mid]==key){
+                return mid;
+            }
+            if(array[mid]>key){
+                end = mid-1;
+            }
+            if(array[mid]<key){
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int array[] = {2, 4, 6, 8, 10, 12, 14}; //array should be sorted
+        int key = 1;
+
+        int index = binSearch(array, key);
+        if(index == -1){
+            System.out.println("Number is not present in the array");
+        }
+        else{
+            System.out.println("The number is present at index: "+index);
+        }
+    }
+}
