1746884ad650c0ddf8d2deb8ee80eceb30ff363f
Sohail Ansari <65657371+thecodephilic-guy@users.noreply.github.com>
2023-10-11 09:22:48 +0000

    updated reverseAnArray.java

--- code/Array/reverseAnArray.java
+++ code/Array/reverseAnArray.java
@@ -1,15 +1,15 @@
 public class reverseAnArray{
     public static void arrayRerverse(int numbers[]){
         int start = 0, end = numbers.length-1;
-        while(start != end){
-            int temp = start;
-            start = end;
-            end = temp;
+        while(start < end){
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
 
             start++;
             end--;
         }
-        System.out.println("The reversed array is: ");
+        System.out.print("The reversed array is: ");
         for(int i=0; i<numbers.length; i++){
             System.out.print(numbers[i]+" ");
         }
@@ -18,8 +18,7 @@ public class reverseAnArray{
     }
     public static void main(String[] args){
 
-        int numbers[] = {2, 4, 6, 8, 10};
+        int numbers[] = {2, 4, 8, 10};
         arrayRerverse(numbers);
-
     }
 }
