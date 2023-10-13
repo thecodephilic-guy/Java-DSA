d3f5bc0f0b62c728c9ba842f3222fc7212ab9f14
Sohail Ansari <65657371+thecodephilic-guy@users.noreply.github.com>
2023-10-13 17:25:27 +0000

    solved stocks buying and selling problem

---
+++ code/Array/buyAndSellStocks.java
@@ -0,0 +1,23 @@
+public class buyAndSellStocks {
+
+    public static int sellStocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice;  //today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println("The Maximum profit is: "+sellStocks(prices));
+    }
+}
