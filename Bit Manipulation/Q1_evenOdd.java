ee18624fce433863b1bc7eb98f97fcace7282d5b
thecodephilic-guy <sohailansari483@gmail.com>
2023-10-20 01:23:19 +0530

    Even Odd using bit manipulation

---
+++ Bit Manipulation/Q1_evenOdd.java
@@ -0,0 +1,26 @@
+//Q. Check if a number is Odd or Even
+
+/*
+ * Logic: 
+ *  The LSB of all the binary equivalents of decimals is 1 for odd numbers.
+ *  And LSB of all the binary equivalents of decimals is 0 for even numbers.
+ * 
+ * Now all we need is a method to check the LSB of a number. 
+ * We can make the use of Bitwise AND &. Let's see how:
+ */
+
+import java.util.Scanner;
+
+public class Q1_evenOdd {
+
+    public static void oddEven(int number){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        sc.close();
+        int number = sc.nextInt();
+        
+    }
+}
