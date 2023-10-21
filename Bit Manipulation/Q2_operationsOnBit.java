b7ca03612a46e0d3c20968b26da66d84419be82c
thecodephilic-guy <sohailansari483@gmail.com>
2023-10-21 14:52:41 +0530

    Added few more operations

--- Bit Manipulation/Q2_operationsOnBit.java
+++ Bit Manipulation/Q2_operationsOnBit.java
@@ -14,14 +14,14 @@ class operations{
     //Operation - 2: Set ith bit:
     public int setBit(int n, int i){
         int bitMask = 1<<i;
-        System.out.print("The number after setting the "+i+"th bit is: ");
+        System.out.print(n+" after setting the "+i+"th bit is: ");
         return (n | bitMask);
     }
 
     //Operation - 3: Clear ith bit:
     public int clearBit(int n, int i){
         int bitMask = ~(1<<i);
-        System.out.print("The number after clearing the "+i+"th bit is: ");
+        System.out.print(n+" after clearing the "+i+"th bit is: ");
         return (n & bitMask);
     }
 
@@ -41,6 +41,28 @@ class operations{
          * return (n | bitMask);
          */
     }
+
+    //Operation - 5: Clear last i bits:
+    public int clearIbits(int n, int i){
+        int bitMask = ~0 << i;
+        return n & bitMask;
+    }
+
+    //Operation - 6: Clear a range of bits:
+    public int clearRangeOfBits(int n, int i, int j){
+        int bitMask = (~0 << (j+1) | ((1 << i) - 1));              
+        return (n & bitMask);
+    }
+    /*
+     *      as,
+     *      01 = (2 raised to the power 1) - 1
+     *     011 = (2 raised to the power 2) - 1
+     *    0111 = (2 raised to the power 3) - 1
+     *   01111 = (2 raised to the power 4) - 1
+     *  011111 = (2 raised to the power 5) - 1
+     * 
+     * gerneral: binary number with all 1s on right = (2 raised to the power number of 1s on the right) - 1
+     */
 }
 
 
@@ -51,6 +73,8 @@ public class Q2_operationsOnBit {
         System.out.println(bitObj.setBit(10, 0));
         System.out.println(bitObj.clearBit(10, 1));
         System.out.println(bitObj.updateBit(10, 0, 1));
+        System.out.println("Number after clearing i bits: "+bitObj.clearIbits(15, 2));
+        System.out.println("Number after clearing a range of bits: "+bitObj.clearRangeOfBits(2515, 2, 7));
     }
 }
 
