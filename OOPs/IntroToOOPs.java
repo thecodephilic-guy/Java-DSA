20f7701fba61aaa1d01e23d68d15be57f2dfe760
thecodephilic-guy <sohailansari483@gmail.com>
2023-10-23 13:25:22 +0530

    Added access specifier to the code

--- OOPs/IntroToOOPs.java
+++ OOPs/IntroToOOPs.java
@@ -5,27 +5,42 @@ public class IntroToOOPs {
         Pen newPen = new Pen();
 
         newPen.setColor("Red");
-        System.out.println(newPen.color);
+        // System.out.println(newPen.color); //this will trough error if acces specifier is private
 
         newPen.setTip(7);
-        System.out.println(newPen.tip);
+        // System.out.println(newPen.tip);  //this will trough error if acces specifier is private
+
+        // newPen.color = "Black";     //This prop is changable from main function (A security issue)
+        
+        //We need to know about access modifiers
+        
+        System.out.println(newPen.getColor());
+        System.out.println(newPen.getTip());
     }
 }
 
 class Pen{
 
-    String color;
-    int tip;
+    private String color;
+    private int tip;
 
     Pen(){
         color = "Blue";
     }
 
-    void setColor(String newName){
-        this.color = newName;
+    void setColor(String color){     //setter method
+        this.color = color;
     }
 
-    void setTip(int tip){
+    void setTip(int tip){           //setter method
         this.tip = tip;
+    }
+
+    String getColor(){          //getter method
+        return this.color;
+    }
+
+    int getTip(){           //getter method
+        return this.tip;
     }
 }
