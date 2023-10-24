c2365e3e86c524a06468043d86fe1a2edc253c6f
thecodephilic-guy <sohailansari483@gmail.com>
2023-10-24 12:54:55 +0530

    Added Intro file

--- OOPs/IntroToOOPs.java
+++ OOPs/IntroToOOPs.java
@@ -16,6 +16,10 @@ public class IntroToOOPs {
         
         System.out.println(newPen.getColor());
         System.out.println(newPen.getTip());
+
+        Pen copyPen = new Pen(newPen);
+        System.out.println(copyPen.getColor());
+        System.out.println(copyPen.getTip());
     }
 }
 
@@ -23,6 +27,26 @@ class Pen{
 
     private String color;
     private int tip;
+
+    //this is called constructor. It is called at the time of creation of a new object.
+    //There are 3 types of constructors:
+    //  Non-parameterised
+    //  Parameterised
+    //  Copy
+
+    /*
+        Let's read about copy constructor:
+        The work of a copy constructor is to copy the properties of one object to another object
+     */
+
+
+    //Shallow copy constructor if array was involed
+    Pen(Pen newPen){    //Passing an object of same class as parameter
+        this.color = newPen.color;
+        this.tip = newPen.tip;
+    }
+
+    //Deep copy constructor:
 
     Pen(){
         color = "Blue";
